6ef08481552f61f7b9f2fb23b05025bd540dc487
Judd Maltin <judd@newgoliath.com>
2018-05-31 15:54:39 -0400

    synergy restart

--- home/synergy_restart.sh
+++ home/synergy_restart.sh
@@ -1,1 +1,3 @@
-../../dotfiles/home/synergy_restart.sh
+launchctl unload /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
+killall synergy
+launchctl load /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
